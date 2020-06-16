c98bc15165bf9b16baf208517456f9b960d71221
Yoonjunhyeon <haring157@khu.ac.kr>
2020-06-17 05:23:22 +0900

    t

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   lintOnSave: true,
   transpileDependencies: ['vuetify'],
-
-  // options...
-  // devServer: {
-  //   '^/api/': {
-  //     target: 'http://127.0.0.1:8000/api/',
-  //   },
-  // },
-  // outputDir: './dist/',
-  // // assetsDir must match Django's STATIC_URL
-  // assetsDir: 'static',
+  configureWebpack: {
+    output: {
+      path: `${__dirname}/cool-build`,
+    },
+  },
+  devServer: {
+    disableHostCheck: true,
+  },
 };
