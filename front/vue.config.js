31d734233e71aae9cdf719cbef151a9bc7715262
Yoonjunhyeon <haring157@khu.ac.kr>
2020-06-17 05:48:13 +0900

    vue.config 수정

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   lintOnSave: true,
   transpileDependencies: ['vuetify'],
-  configureWebpack: {
-    output: {
-      path: `${__dirname}/cool-build`,
-    },
-  },
   devServer: {
     disableHostCheck: true,
   },
